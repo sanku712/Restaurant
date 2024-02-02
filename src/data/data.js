198662dcb6046346917241b5a882165d2cad2ed2
Sanket Patil <sanketdadapatil@gmail.com>
2024-02-03 03:13:09 +0530

    first message

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+import Dosa from '../images/dosa.jpg'
+import chhola from '../images/chhola.jpg'
+import Idli from '../images/idli.jpg'
+import MasalaDosa from '../images/masala.jpg'
+import Paneer from '../images/paneer.jpg'
+import Gujrati from '../images/gujrati.jpeg'
+
+export const MenuList = [
+    {
+        name: 'Dosa',
+        description: 'A dosa is South Indian, fermented crepe made from rice batter and black lentils. Masala Dosa, specifically, is when you stuff it with a lightly cooked filling of potatoes, fried onions and spices.',
+        image:Dosa,
+        price:200
+    },
+    {
+        name: 'chola',
+        description: 'Chole bhature is a food dish popular in the northern areas of the Indian subcontinent. It is a combination of chana masala (spicy white chickpeas) and bhatura/puri, a deep-fried bread made from maida.',
+        image:chhola,
+        price:250
+    },
+    {
+        name: 'Idli Sambhar',
+        description: 'Idli Sambar is a South Indian breakfast meal where soft fluffy steamed cakes known as idli are served with sambar, a vegetable lentil stew. Idli Sambar is made with lentils, plenty of spices, mixed vegetables and herbs.',
+        image:Idli,
+        price:200
+    },
+    {
+        name: 'Masala Dosa ',
+        description: 'Masala Dosa is a popular South Indian breakfast where a crispy crepe made of fermented rice and lentil batter is served with flavorful spiced potato curry. It is a wholesome meal in itself as it is served with potato masala, Coconut chutney and Sambar.',
+        image:MasalaDosa,
+        price:160
+    },
+    {
+        name: 'Paneer',
+        description: 'Paneer is an Indian cheese that s made from curdled milk and some sort of fruit or vegetable acid like lemon juice. There are some distinct traits that make Paneer the cheese that it is.',
+        image:Paneer,
+        price:350
+    },
+    {
+        name: 'Gujrati',
+        description: 'Gujarati thali prepared in Gujarati households has at least three fresh vegetable dishes, one dry dal or some sprouted pulses dish (ugaadayla mung, for instance), a wet dal, kadhi, kathor (a savoury), mithai, poori, rotis, steamed rice, chaash and papad. Kathiawadi Thali is a variation of Gujarati Thali.',
+        image:Gujrati,
+        price:549
+    }
+]
